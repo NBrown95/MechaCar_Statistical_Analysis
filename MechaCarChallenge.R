c3387711019d8bbf74d3ecb7a5a242b4dfa57713
NBrown95 <natebrown918@gmail.com>
2021-07-30 22:06:50 -0500

    Adding t-test results.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,8 @@ coils <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 total_summary <- coils %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep') #get summary statistics
 lot_summary <- coils %>% group_by(Manufacturing_Lot) %>% 
   summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep') #group summary by manufacturing lots
+
+t.test(coils$PSI,mu=1500) #t test for all lots
+t.test(subset(coils,Manufacturing_Lot=='Lot1')$PSI,mu=1500) #using subset to get t test of each lot
+t.test(subset(coils,Manufacturing_Lot=='Lot2')$PSI,mu=1500)
+t.test(subset(coils,Manufacturing_Lot=='Lot3')$PSI,mu=1500)
