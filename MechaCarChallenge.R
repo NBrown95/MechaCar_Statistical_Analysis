00908f0e704e281351e1cb2b699a33369dc53dee
NBrown95 <natebrown918@gmail.com>
2021-07-30 20:56:31 -0500

    Adding summary table for coils dataset.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,3 +4,8 @@ lm(mpg ~ + vehicle_length + vehicle_weight + spoiler_angle +
      ground_clearance + AWD,data=table) #generate multiple linear regression model
 summary(lm(mpg ~ + vehicle_length + vehicle_weight + spoiler_angle +
              ground_clearance + AWD,data=table)) #get summary
+
+coils <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- coils %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep') #get summary statistics
+lot_summary <- coils %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep') #group summary by manufacturing lots
